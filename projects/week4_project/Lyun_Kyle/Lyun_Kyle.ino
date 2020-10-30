92300431ba109007840dfe939ca1fd0e19a472ce
aspboy217 <72766090+aspboy217@users.noreply.github.com>
2020-10-29 23:44:34 -0700

    Update Lyun_Kyle.ino
    Finished TODO in the codes

--- projects/week4_project/Lyun_Kyle/Lyun_Kyle.ino
+++ projects/week4_project/Lyun_Kyle/Lyun_Kyle.ino
@@ -15,9 +15,11 @@ void setup() {
   // ==========================================================================================
   // TODO: Intialize three PWM output pins and one analog input pin. Makesure the pins selected 
   //       have PWM capabilities.
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
   
-  
-  
+  pinMode(A1, INPUT); // we are not using this
   // ==========================================================================================
 }
 
@@ -33,20 +35,30 @@ void loop() {
     output = map(val, 0, 341, 0, 255);
     // TODO: Output the mapped PWM onto the LED. LED 2 and LED 3 should be OFF
     //       LED 1 will vary depending on analog input
-    
+    analogWrite(9, 0.5 * val);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
   }
 
   // If you are unfamiliar with C++, && means that both conditions need to be satisfied.
   if (val >= 342 && val <= 682) { // LED 2
     // TODO: Map out the val of the potentiometer and output the mapped PWM onto the LED.
     //       LED 1 should be fully lit. LED 3 should be off. LED 2 will vary depending on analog input
+    output = map(val, 342, 682, 0, 255);
     
+    digitalWrite(9, HIGH);
+    analogWrite(10, 0.3 * val);
+    digitalWrite(11, LOW);
   }
   
   
-  if (val >=  && val <= ) { // LED 2
+  if (val >= 683 && val <= 1023) { // LED 2
     // TODO: Insert missing values of if statement above.
     //       Map out the val of the potentiometer and output the mapped PWM onto the LED.
     //       LED 1 and 2 should be fully lit. LED 3 will vary depending on analog output
+    output = map(val, 683, 1023, 0, 255);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    analogWrite(11, val);
   }
 }
