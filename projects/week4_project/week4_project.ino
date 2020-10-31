d08fb13764e2b8a43783e7c523959488ea12bc88
Jeff1310 <73588127+Jeff1310@users.noreply.github.com>
2020-10-30 18:58:34 -0700

    Update week4_project.ino

--- projects/week4_project/week4_project.ino
+++ projects/week4_project/week4_project.ino
@@ -1,7 +1,14 @@
+
+//jeff
+
 // Complete the following TODOs for this program, then upload to your board.
 // Correction in the lecture video: we provided a table for 
 // analog inputs -> expected output, but provided the wrong values
 // analog inputs should be the following: 0 - 341, 342 - 682, and 683 - 1023
+int led_pin1 = 10;
+int led_pin2 = 5;
+int led_pin3 = 3;
+
 
 void setup() {
   // Anything written after "//" on the same line is a comment in C++
@@ -15,7 +22,9 @@ void setup() {
   // ==========================================================================================
   // TODO: Intialize three PWM output pins and one analog input pin. Makesure the pins selected 
   //       have PWM capabilities.
-  
+  pinMode(led_pin1,OUTPUT);
+  pinMode(led_pin2,OUTPUT);
+  pinMode(led_pin3,OUTPUT);
   
   
   // ==========================================================================================
@@ -25,6 +34,8 @@ void loop() {
   // Any code you put here (in this function) will be looped (ran over and over again)
   int val = analogRead(A0);
   int output = 0;
+  int o1 = 342;
+  int o2 = 683;
 
 
   // If you are unfamiliar with C++, this statement checks if the condition inside the parenthesis
@@ -32,6 +43,9 @@ void loop() {
   if (val < 342) { // LED 1
     output = map(val, 0, 341, 0, 255);
     // TODO: Output the mapped PWM onto the LED. LED 2 and LED 3 should be OFF
+   
+    analogWrite(led_pin3, output);
+  
     //       LED 1 will vary depending on analog input
     
   }
@@ -39,14 +53,21 @@ void loop() {
   // If you are unfamiliar with C++, && means that both conditions need to be satisfied.
   if (val >= 342 && val <= 682) { // LED 2
     // TODO: Map out the val of the potentiometer and output the mapped PWM onto the LED.
+    o1 = map(val, 342, 682, 0, 255);
+    analogWrite(led_pin1, o1);
+    
     //       LED 1 should be fully lit. LED 3 should be off. LED 2 will vary depending on analog input
     
   }
   
   
-  if (val >=  && val <= ) { // LED 2
+  if (val >= 683 && val <= 1023) { // LED 2
     // TODO: Insert missing values of if statement above.
     //       Map out the val of the potentiometer and output the mapped PWM onto the LED.
+    o2 = map(val, 683, 1023, 0, 255);
+    analogWrite(led_pin2, o2);
+    
     //       LED 1 and 2 should be fully lit. LED 3 will vary depending on analog output
   }
-}
+  
+} 
