721327141c2246e660714041bd02a62130730950
therealhuy <32783643+therealhuy@users.noreply.github.com>
2020-11-01 14:05:18 -0800

    Updated comments to be more clear
    Title

--- projects/week5_project/week5_project.ino
+++ projects/week5_project/week5_project.ino
@@ -1,17 +1,24 @@
+// These variables are used for your state machine
 int time_start = 0;
 int state = 0;
+
 void setup() {
   // TODO: Initialize pins for potentiometer, 2 LEDs (PWM is optional), 
   //       and transistor control (PWM). Please use D3 as button.
   Serial.begin(9600);
   pinMode(3,INPUT);
+
+  
 }
 
 void loop() { 
+  // ================================================
+  // DO NOT TOUCH THIS SECTION OF CODE
   // Implementation of state machine with software button debouncing
-   // 0 for off, 1 for on
   int button_press = digitalRead(3); // Reads HIGH if pressed, LOW if not pressed
   int time_elapsed = millis() - time_start;
+  // Change states only if 1 second has passed (this prevents multiple state changes from one press)
+  // Long presses can change states multiple times if held down for more than one second.
   if (button_press == HIGH && time_elapsed >= 1000) {
     time_start = millis();
     if (state == 0) {
@@ -23,10 +30,12 @@ void loop() {
       Serial.println("OFF");
     }
   }
-  
+  // DO NOT TOUCH ABOVE SECTION OF CODE
+  // ================================================
+
+
   // TODO: Using the state variable given, create a program that meets all project specifications
   //       The conditional statements are written for you.
-
   if (state == 0) {
     // TODO: Things that should happen if the state is "off"
 
